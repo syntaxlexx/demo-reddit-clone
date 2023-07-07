2cdd9143cf88c42c2ccab6b0b6cb82dedfa47239
SyntaxLexx <spencermainaa@gmail.com>
2023-07-07 19:58:46 +0300

    feat: sign in page

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { FC, useState } from "react";
+import { Button } from "./ui/Button";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      // toast notification
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+      >
+        {!isLoading && <Icons.google className="h-4 w-4 mr-2"></Icons.google>}
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
